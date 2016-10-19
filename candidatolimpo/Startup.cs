0d8a92a2d005d3ebe271c6c7ca36cd53a1a76b9c
Gleidson <Gleidson@FOX-RIVER>
2016-10-18 23:13:32 -0200

    Commit inicial

---
+++ candidatolimpo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(candidatolimpo.Startup))]
+namespace candidatolimpo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
